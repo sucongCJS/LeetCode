b2e43e01fd4533616b31de3d8ec0314ff33ad3a3
jason <sucongjs@gmail.com>
2018-12-13 18:09:55 +0800

    indent problem

--- Queue_Stack/Queue/queue.cpp
+++ Queue_Stack/Queue/queue.cpp
@@ -3,35 +3,35 @@
 using namespace std;
 
 class MyQueue {
-    private:
-        // store elements
-        vector<int> data;       
-        // a pointer to indicate the start position
-        int p_start;            
-    public:
-        MyQueue() {p_start = 0;}
-        /** Insert an element into the queue. Return true if the operation is successful. */
-        bool enQueue(int x) {
-            data.push_back(x);
-            return true;
+private:
+    // store elements
+    vector<int> data;       
+    // a pointer to indicate the start position
+    int p_start;            
+public:
+    MyQueue() {p_start = 0;}
+    /** Insert an element into the queue. Return true if the operation is successful. */
+    bool enQueue(int x) {
+        data.push_back(x);
+        return true;
+    }
+    /** Delete an element from the queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if (isEmpty()) {
+            return false;
         }
-        /** Delete an element from the queue. Return true if the operation is successful. */
-        bool deQueue() {
-            if (isEmpty()) {
-                return false;
-            }
-            // data.push_back;数据才会被删除, 下面只是把指针前移
-            p_start++;
-            return true;
-        };
-        /** Get the front item from the queue. */
-        int Front() {
-            return data[p_start];
-        };
-        /** Checks whether the queue is empty or not. */
-        bool isEmpty()  {
-            return p_start >= data.size();
-        }
+        // data.push_back;数据才会被删除, 下面只是把指针前移
+        p_start++;
+        return true;
+    };
+    /** Get the front item from the queue. */
+    int Front() {
+        return data[p_start];
+    };
+    /** Checks whether the queue is empty or not. */
+    bool isEmpty()  {
+        return p_start >= data.size();
+    }
 };
 
 int main() {
